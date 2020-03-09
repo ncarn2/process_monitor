aacd1c108d310c94b218bc6609d186c24238668e
Nicholas Carnival <carnivalnicholas2@gmail.com>
2020-03-09 13:52:22 -0600

    top lists everything in /proc

--- src/top.c
+++ src/top.c
@@ -1,6 +1,32 @@
+#include <dirent.h>
+#include <limits.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-    printf("Hello World\n");
+
+    char cwd[PATH_MAX];
+
+    if (chdir("/proc"))
+    {
+        printf("chdir() to /proc failed\n");
+        return 1;
+    } 
+
+    int failed = read_proc();
+
+    return 0;
+}
+
+int read_proc() {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("/proc");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
     return 0;
 }
